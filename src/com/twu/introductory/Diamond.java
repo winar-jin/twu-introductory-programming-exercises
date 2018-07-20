c17bfd0e7face147b4ed6015ee784cbb2a1945a5
winar-jin <winar1994@outlook.com>
2018-07-20 12:19:19 +0800

    [feat] Diamond centered method

--- src/com/twu/introductory/Diamond.java
+++ src/com/twu/introductory/Diamond.java
@@ -1,9 +1,11 @@
 package com.twu.introductory;
 
+import java.util.Arrays;
+
 /**
  * Created by jin on 20/07/2018.
  */
-public class Diamond {
+class Diamond {
 
   String horizontal(int n) {
     StringBuilder result = new StringBuilder();
@@ -21,6 +23,18 @@ public class Diamond {
       }
       result.append(horizontal(i));
       if (i + 1 != n) result.append("\n");
+    }
+    return result.toString();
+  }
+
+  String centered(int n) {
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < 2 * n - 1; i++) {
+      for (int j = 0; j < Math.abs(n - i - 1); j++) {
+        result.append(" ");
+      }
+      result.append(horizontal(n - 1 - Math.abs(n - i - 1)));
+      if (i + 1 != 2 * n - 1) result.append("\n");
     }
     return result.toString();
   }
--- test/com/twu/introductory/DiamondTest.java
+++ test/com/twu/introductory/DiamondTest.java
@@ -20,4 +20,10 @@ public class DiamondTest {
   public void should_return_a_isosceles_triangle_with_isosceles_method() {
     assertEquals("  *\n ***\n*****", diamond.isosceles(3));
   }
+
+  @Test
+  public void should_return_a_centered_diamond_with_centered_method() {
+    assertEquals("  *\n ***\n*****\n ***\n  *", diamond.centered(3));
+  }
+
 }
