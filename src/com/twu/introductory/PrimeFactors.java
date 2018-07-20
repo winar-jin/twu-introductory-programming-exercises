bcddf59e1f5d50a96eb088400cf58b93dd0b33af
winar-jin <winar1994@outlook.com>
2018-07-20 12:55:09 +0800

    [feat] PrimeFactors simple return empty array

---
+++ src/com/twu/introductory/PrimeFactors.java
@@ -0,0 +1,12 @@
+package com.twu.introductory;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jin on 20/07/2018.
+ */
+class PrimeFactors {
+  ArrayList<Integer> generate(int n) {
+    return new ArrayList<Integer>();
+  }
+}
---
+++ test/com/twu/introductory/PrimeFactorsTest.java
@@ -0,0 +1,26 @@
+package com.twu.introductory;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jin on 20/07/2018.
+ */
+public class PrimeFactorsTest {
+  static PrimeFactors primeFactors;
+
+  @BeforeClass
+  static public void beforeClass() {
+    primeFactors = new PrimeFactors();
+  }
+
+  @Test
+  public void should_return_empty_list_when_generate_1() {
+    ArrayList<Integer> expect = new ArrayList<Integer>();
+    assertEquals(expect, primeFactors.generate(1));
+  }
+}
